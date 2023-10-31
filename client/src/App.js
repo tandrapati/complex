3bc58b4656c78fcfc152441ff92e5ef2e5465998
Vk Tandrapati <vk.tandrapati@veeva.com>
2023-10-31 09:55:40 -0500

    test deployment

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,8 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/">Home-updated</Link>
+          <Link to="/otherpage">Other Page updated</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
